9a84dfb9a409d25a06c6f1fc940f02468735ebf9
Michael Zheng <michaelzheng54@hotmail.com>
2020-01-15 23:48:09 +0000

    Updates

---
+++ precache-manifest.c79e7b1de31b43841c9907958b0a758e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba7ba31634b17798cfee8db11a332556",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8061bd14a722a32cde9",
+    "url": "/static/css/main.60e41e81.chunk.css"
+  },
+  {
+    "revision": "8ee2ab029459948c23b9",
+    "url": "/static/js/2.f0d82991.chunk.js"
+  },
+  {
+    "revision": "fd56044fbab90a74509299bbf81211c5",
+    "url": "/static/js/2.f0d82991.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d8061bd14a722a32cde9",
+    "url": "/static/js/main.0c93d436.chunk.js"
+  },
+  {
+    "revision": "b2a7aca4a36d63d5109b",
+    "url": "/static/js/runtime-main.a87feddb.js"
+  },
+  {
+    "revision": "825a2cdd28cf7a9b7a17c70b13f74630",
+    "url": "/static/media/hermes.825a2cdd.png"
+  },
+  {
+    "revision": "71810740831694f96501e97b73042fbf",
+    "url": "/static/media/pigeon.71810740.png"
+  },
+  {
+    "revision": "5b5cb9f10f248f9c9287d78327e2a51e",
+    "url": "/static/media/teacher-square.5b5cb9f1.png"
+  }
+]);
